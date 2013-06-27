c8d54ab9924874dc876e951302088ae61d399607
akaloaderadmin@gmail.com <akaloaderadmin@gmail.com@a2f6c882-ab1d-ed56-34ce-2e8b3e3fac5e>
2013-06-27 17:04:31 +0000

    add support to modules...

--- trunk/akernelloader/start_kernel_ak.c
+++ trunk/akernelloader/start_kernel_ak.c
@@ -33,6 +33,8 @@ typedef void (*entry_func) (int, int, int, int, int, int)
 
 entry_func entry_addr;
 
+static struct multiboot_mod_list module_list[MULTIBOOT_MODULE_NUM];
+static int mod_cur_addr;
 
 
 int multiboot_probe_ak(void* kernel_address, __u32 *mb_offset)
@@ -142,18 +144,64 @@ struct linux_kernel_header *linux_h;
        if(IS_ELF(kernel_address))  
            load_elf(kernel_address,&entry);
          
+        int i;
+        for(i=0;i < MULTIBOOT_MODULE_NUM;i++){
+              if( strlen_akel (module_path[i]) == 0)
+                  break;
+              if(i == 0){
+                mod_cur_addr = entry + kernel_size ;
+                mb_load_module (module_path[i]);
+               }
+
+               mb_load_module (module_path[i]);
+
+          }
        
            boot_kernel_ak ((int)entry, (int)&mbinfo ); 
          
 }
 
+int mb_load_module (char *module)
+{
+  int len;
+
+  /* if we are supposed to load on 4K boundaries */
+  mod_cur_addr = (mod_cur_addr + 0xFFF) & 0xFFFFF000;
+
+   struct FILE *fd;
+
+   fd = fopen_ak(module);
+  
+   if(fd == NULL)
+       printf_ak("\n Can not open module file ...\n");
+ 
+  len = len = fread_ak( mod_cur_addr,fd->filelength, 1, fd );
+  if (! len)
+    {
+        fs_op_ak.close(fd);
+      return 0;
+    }
+
+  printf_ak("   [Multiboot-module @ 0x%x, 0x%x bytes]\n", mod_cur_addr, len);
+
+  /* these two simply need to be set if any modules are loaded at all */
+  mbinfo.flags |= MULTIBOOT_INFO_MODS ;
+  mbinfo.mods_addr = (int) module_list;
+  module_list[mbinfo.mods_count].mod_start = mod_cur_addr;
+  mod_cur_addr += len;
+  module_list[mbinfo.mods_count].mod_end = mod_cur_addr;
+  module_list[mbinfo.mods_count].pad = 0;
+
+  /* increment number of modules included */
+  mbinfo.mods_count++;
+
+   fs_op_ak.close(fd);
+  return 1;
+}
+
+
+
 int load_linux(__u32 kernel_size,const char * kernel_path){
-
-
-
-
-
-
 
    struct linux_kernel_header *linux_h;
    __u32 setup_size , linux_start_addr;
@@ -171,17 +219,18 @@ int load_linux(__u32 kernel_size,const char * kernel_path){
   linux_h->type_of_loader= 0x70 ;
  int setup_sects = linux_h->setup_sects;
  char * cmd_line= COMMAND_LINE_OFFSET;
+
     /* If sectors setup are not defined , set it to the default (4).  */
       if (! setup_sects)
-	setup_sects = 4 ;
+    setup_sects = 4 ;
 
           setup_size = (setup_sects + 1) * 512 ;
          
-	  linux_h->cl_magic = LINUX_CL_MAGIC;
-	  linux_h->cl_offset = cmd_line - linux_base_ptr;
-	  kernel_size -= setup_size;
+      linux_h->cl_magic = LINUX_CL_MAGIC;
+      linux_h->cl_offset = cmd_line - linux_base_ptr;
+      kernel_size -= setup_size;
 
-	     
+         
           linux_start_addr = (char *) LINUX_KERNEL_ADDR ;      
           linux_base_ptr  = LINUX_SETUP_ADDR;
           
@@ -207,8 +256,9 @@ int load_linux(__u32 kernel_size,const char * kernel_path){
         return 1 ;
 
 }
-int
-load_initrd (const char *initrd_path)
+
+
+int load_initrd (const char *initrd_path)
 {
   
   unsigned long addr_initrd, len ;
@@ -225,7 +275,7 @@ load_initrd (const char *initrd_path)
     intrd_size = fd->filelength;
      
   initrd_max = (linux_h->header == LINUX_MAGIC_SIGNATURE && linux_h->version >= 0x0203
-	      ? linux_h->initrd_addr_max : LINUX_INITRD_MAX_ADDRESS );
+          ? linux_h->initrd_addr_max : LINUX_INITRD_MAX_ADDRESS );
   
    
 
